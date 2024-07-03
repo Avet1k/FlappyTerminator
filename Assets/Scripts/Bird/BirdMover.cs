7071b3bdd5bb0bdca9c792dbcc50b8bf7c82b543
Avetik <xavetikx@gmail.com>
2024-07-03 12:18:32 +0300

    Mover and Tracker are ready

---
+++ Assets/Scripts/Bird/BirdMover.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class BirdMover : MonoBehaviour
+{
+    [SerializeField] private float _flapForce;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _maxRotationZ;
+    [SerializeField] private float _minRotationZ;
+
+    private Rigidbody2D _rigidbody2D;
+    private Vector3 _startPosition;
+    private Quaternion _maxRotation;
+    private Quaternion _minRotation;
+    private KeyCode _jump = KeyCode.Space;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _startPosition = transform.position;
+        _maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
+        _minRotation = Quaternion.Euler(0, 0, _minRotationZ);
+
+        Reset();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(_jump))
+        {
+            _rigidbody2D.velocity = new Vector2(_speed, _flapForce);
+            transform.rotation = _maxRotation;
+        }
+        
+        transform.rotation = Quaternion.Lerp(transform.rotation, _minRotation, _rotationSpeed * Time.deltaTime);
+    }
+
+    public void Reset()
+    {
+        transform.position = _startPosition;
+        transform.rotation = Quaternion.identity;
+        _rigidbody2D.velocity = Vector2.zero;
+    }
+}
